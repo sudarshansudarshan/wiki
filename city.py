81199833cb1f81cf8e0b71cc55910a2cca65a4c9
S. R. S. Iyengar <sudarshan@iitrpr.ac.in>
2022-10-17 17:46:53 +0530

    added city.py

---
+++ city.py
@@ -0,0 +1,21 @@
+"""
+This python program will consider a city and extract all the proper nouns from
+the city and create a list of words with their time of arrival, in the revision
+history. 
+
+*India is a country in south asia. The place is more of a sub-continent than a
+country. There are 26 languages spoken in the country with *Hindi being one of
+the widely used official language. India got its independence on 15th of August
+1947. Although, many freedom fighters were involved in the freedom struggle,
+*Mahatma *Gandhi is considered to have played a major role. Post Independence,
+the country got sub-divided into states with *Delhi being the capital.
+*Bangladesh and *Pakistan saw itself distancing from the country, thus creating
+the partition of this region for the first time in the documented history 
+
+(There are a total of 7 proper nouns in the above paragraph.) 
+"""
+
+
+
+
+
